fe378b795e75cb4f6f74f1385ae3784b284dc4e2
Eva Lichnovska <ellichnovska@gmail.com>
2018-11-18 23:37:49 +0100

    vecer pisu dalsi radek

--- TestProGit/src/cz/evalich/dalsitest/DalsiTest.java
+++ TestProGit/src/cz/evalich/dalsitest/DalsiTest.java
@@ -5,6 +5,8 @@ public class DalsiTest {
 	public static void main(String[] args) {
 		System.out.println("tohle je jen test");
 		System.out.println("pokracuji v testu");
+		System.out.println("nedele vecer pisu");
+		
 	}
 
 }
