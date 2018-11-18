bc07deb4e67c4a7450a604aea5a24908a2accbe5
Eva Lichnovska <ellichnovska@gmail.com>
2018-11-18 13:55:39 +0100

    v nedeli jsem umazala radek

--- test2/src/cz/evalich/test2/TestEva.java
+++ test2/src/cz/evalich/test2/TestEva.java
@@ -5,12 +5,14 @@ public class TestEva {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		System.out.println("toto je trida na zjisteni, jak funguje git a jeho push a pull a tak");
+		
 		System.out.println("ja jsem Eva a rikam ahoooj a vubec mi po dnesku nehrabe");
 		for (int i = 0; i < 3; i++) {
 			System.out.println("hrab");
 		}
 		System.out.println("v nedeli eva zdravi papa");
+		
+		
 	}
 
 }
